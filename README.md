6913f8fb6a060955a4424ea85d4715e7707b8b73
cybernet <cybernet2u@users.sourceforge.net>
2015-03-06 09:04:51 +0200

    clean

--- README.md
+++
@@ -1,5 +0,0 @@
-Ubuntu repo ( branch )
-==================================
-
-I want to use this repo for Ubuntu tutorials
-It will contain configuration files && bash scripts
--- sh/rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# rename all files matching .jpg to prefix_*1-99*.jpg
-cd /home/user/working_dir
-cnt=0
-for fname in *.jpg
-do
-    mv $fname prefix_${cnt}.jpg
-    cnt=$(( $cnt + 1 ))
-done
