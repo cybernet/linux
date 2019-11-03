070b4d8abac54f80d64e5b75e9dc4de3ea638c45
cybernet <cybernet2u@yahoo.com>
2019-11-03 19:11:02 +0000

    change filename to swap0

--- sh/swap.sh
+++ sh/swap.sh
@@ -3,19 +3,19 @@
 # size of swapfile in megabytes
 swapsize=1024
 
-# does the swap file already exist?
-grep -q "swapfile" /etc/fstab
+# does the swap file already exist ?
+grep -q "swap0" /etc/fstab
 
 # if not then create it
 if [ $? -ne 0 ]; then
-	echo 'swapfile not found. Adding swapfile.'
-	fallocate -l ${swapsize}M /swapfile
-	chmod 600 /swapfile
-	mkswap /swapfile
-	swapon /swapfile
-	echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+	echo 'swap0 not found. Adding swap0.'
+	fallocate -l ${swapsize}M /swap0
+	chmod 600 /swap0
+	mkswap /swap0
+	swapon /swap0
+	echo '/swap0 none swap defaults 0 0' >> /etc/fstab
 else
-	echo 'swapfile found. No changes made.'
+	echo 'swap0 found. No changes made.'
 fi
 
 # output results to terminal
