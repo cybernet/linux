16785d450db75f7eaf81a38a436759bf06ed9a97
cybernet <cybernet2u@yahoo.com>
2020-03-28 21:51:37 +0000

    swapfile var

--- sh/swap2g.sh
+++ sh/swap2g.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# size of swapfile in megabytes
 swapsize=2048
+swapfile=swap0
 
 # does the swap file already exist?
-grep -q "swap2g" /etc/fstab
+grep -q "${swapfile}" /etc/fstab
 
 # if not then create it
 if [ $? -ne 0 ]; then
 	echo 'swapfile not found. Adding swapfile.'
-	fallocate -l ${swapsize}M /swap2g
-	chmod 600 /swap2g
-	mkswap /swap2g
-	swapon /swap2g
-	echo '/swap2g none swap defaults 0 0' >> /etc/fstab
+	fallocate -l ${swapsize}M /${swapfile}
+	chmod 600 /${swapfile}
+	mkswap /${swapfile}
+	swapon /${swapfile}
+	echo '/${swapfile} none swap defaults 0 0' >> /etc/fstab
 else
-	echo 'swapfile found. No changes made.'
+	echo '${swapfile} found. No changes made.'
 fi
 
 # output results to terminal
