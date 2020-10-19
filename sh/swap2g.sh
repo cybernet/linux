b3e598d0aa39c21ab65c0934d8c7ae15d32c4e51
cybernet <cybernet2u@yahoo.com>
2020-10-19 05:11:19 +0100

    vars

--- sh/swap2g.sh
+++ sh/swap2g.sh
@@ -9,13 +9,13 @@ grep -q "${swapfile}" /etc/fstab
 # if not then create it
 if [ $? -ne 0 ]; then
 	echo 'swapfile not found. Adding swapfile.'
-	fallocate -l ${swapsize}M /${swapfile}
-	chmod 600 /${swapfile}
-	mkswap /${swapfile}
-	swapon /${swapfile}
-	echo '/${swapfile} none swap defaults 0 0' >> /etc/fstab
+	fallocate -l ${swapsize}M /$swapfile
+	chmod 600 /$swapfile
+	mkswap /$swapfile
+	swapon /${wapfile
+	echo '/$swapfile none swap defaults 0 0' >> /etc/fstab
 else
-	echo '${swapfile} found. No changes made.'
+	echo '$swapfile found. No changes made.'
 fi
 
 # output results to terminal
