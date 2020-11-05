cf37c941fa5ba409759a0c93b9c24311f7b01d54
cybernet <cybernet2u@yahoo.com>
2020-11-05 14:05:27 +0000

    double quotes

--- sh/swap2g.sh
+++ sh/swap2g.sh
@@ -13,9 +13,9 @@ if [ $? -ne 0 ]; then
 	chmod 600 /$swapfile
 	mkswap /$swapfile
 	swapon /$swapfile
-	echo '/"$swapfile" none swap defaults 0 0' >> /etc/fstab
+	echo "/${swapfile} none swap defaults 0 0" >> /etc/fstab
 else
-	echo '$swapfile found. No changes made.'
+	echo "${swapfile} found. No changes made."
 fi
 
 # output results to terminal
